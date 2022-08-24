bbdc04557b4367f2dd12c7fb243684aba3f7e3df
Knikkey <102648803+Knikkey@users.noreply.github.com>
2022-08-24 15:55:55 +0900

    8/24

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+
+import Title from "./component/Title";
+import "./App.css";
 
 function App() {
+  const [showEvents, setShowEvents] = useState(true);
+
+  const [events, setEvents] = useState([
+    { title: "mario's birthday bash", id: 1 },
+    { title: "bowser's live stream", id: 2 },
+    { title: "race on moo moo farm", id: 3 },
+  ]);
+
+  const handleClick = (id) => {
+    setEvents((prevEvents) => {
+      return prevEvents.filter((event) => id !== event.id);
+    });
+    console.log(id);
+  };
+
+  const subtitle = "All the latest events!";
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Title title="Events In Your Area" subtitle={subtitle} />
+      <Title title="Another title" subtitle="another sub" />
+      {showEvents && (
+        <div>
+          <button onClick={() => setShowEvents(false)}>Hide events</button>
+        </div>
+      )}
+      {showEvents || (
+        <div>
+          <button onClick={() => setShowEvents(true)}>Show events</button>
+        </div>
+      )}
+      {showEvents &&
+        events.map((event) => {
+          return (
+            <div key={event.id}>
+              <h2>{event.title}</h2>
+              <button onClick={() => handleClick(event.id)}>
+                Delete event
+              </button>
+            </div>
+          );
+        })}
     </div>
   );
 }
---
+++ src/component/Title.js
@@ -0,0 +1,20 @@
+export default function Title(props) {
+  return (
+    <div>
+      <h1 className="title">{props.title}</h1>
+      <br />
+      <h2 className="subtitle">{props.subtitle}</h2>
+    </div>
+  );
+}
+
+//Destructuring example
+// export default function Title({title, subtitle}) {
+//     return (
+//       <div>
+//         <h1 className="title">{title}</h1>
+//         <br />
+//         <h2 className="subtitle">{subtitle}</h2>
+//       </div>
+//     );
+//   }
